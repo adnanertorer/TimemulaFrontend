13aa625f611246b7d46b6b3fffdd4f07282064e1
Adnan Ertörer <adnan.ertorer@indata.com.tr>
2026-05-07 21:35:49 +0300

    #feat: health

--- src/app/views/base/disease-main/disease-main.component.ts
+++ src/app/views/base/disease-main/disease-main.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app';
 import { DiseaseMainModel } from 'src/app/shared/model/disease-main-model';
 import { DiseaseMainService } from 'src/app/shared/services/disease-main.service';
 import Constants from 'src/app/shared/tools/constants';
@@ -10,21 +11,28 @@ import Constants from 'src/app/shared/tools/constants';
 @Component({
   selector: 'app-disease-main',
   templateUrl: './disease-main.component.html',
-  styleUrls: ['./disease-main.component.css']
+  styleUrls: ['./disease-main.component.css'],
 })
 export class DiseaseMainComponent implements OnInit {
-
-  model: DiseaseMainModel;
+  model: DiseaseMainModel | undefined;
   list: DiseaseMainModel[] = [];
 
   buttonText: string = Constants.Save;
-  displayColums: string[] = ['diseaseCategoryName', 'isActive','id'];
-  dataSource: MatTableDataSource<DiseaseMainModel>;
+  displayColums: string[] = ['diseaseCategoryName', 'isActive', 'id'];
+  dataSource: MatTableDataSource<DiseaseMainModel> | undefined;
 
-  @ViewChild('paginatorDiseaseMain') paginator: MatPaginator;
-  @ViewChild('diseaseMainSort') sort: MatSort;
+  @ViewChild('paginatorDiseaseMain') paginator: MatPaginator | undefined;
+  @ViewChild('diseaseMainSort') sort: MatSort | undefined;
 
-  constructor(private service: DiseaseMainService, private router: Router) { }
+  constructor(
+    private service: DiseaseMainService,
+    private router: Router,
+    private authService: AuthService,
+  ) {}
+
+  canAccess(permissionCode: string): boolean {
+    return this.authService.hasPermission(permissionCode);
+  }
 
   ngOnInit() {
     this.buttonText = Constants.Save;
@@ -33,63 +41,71 @@ export class DiseaseMainComponent implements OnInit {
       createdBy: 0,
       diseaseCategoryName: '',
       id: 0,
-      isActive: true
+      isActive: true,
     };
     this.getList();
   }
 
-  getList(){
-    this.service.getList().subscribe((data)=>{
-      if(data.success){
+  getList() {
+    this.service.getList().subscribe((data) => {
+      if (data.success) {
         this.list = data.dynamicClass as DiseaseMainModel[];
         this.dataSource = new MatTableDataSource(this.list);
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
+        if (this.paginator) {
+          this.dataSource.paginator = this.paginator;
+        }
+        if (this.sort) {
+          this.dataSource.sort = this.sort;
+        }
       }
-    })
+    });
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+    if (this.dataSource) {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
     }
   }
 
-  getDetail(id){
-    this.service.getDetails(id).subscribe((data)=>{
-      if(data.success){
+  getDetail(id: number) {
+    this.service.getDetails(id).subscribe((data) => {
+      if (data.success) {
         this.model = data.dynamicClass as DiseaseMainModel;
         this.buttonText = Constants.Update;
       }
-    })
+    });
   }
 
-  add(){
-    if(this.model.id == 0){
-      this.service.add(this.model).subscribe((data)=>{
-        if(data.success){
+  add() {
+    if (this.model && this.model.id == 0) {
+      this.service.add(this.model).subscribe((data) => {
+        if (data.success) {
           alert(data.clientMessage);
           this.ngOnInit();
         }
-      })
-    }else{
-      this.service.update(this.model).subscribe((data)=>{
-        if(data.success){
+      });
+    } else if (this.model) {
+      this.service.update(this.model).subscribe((data) => {
+        if (data.success) {
           alert(data.clientMessage);
           this.ngOnInit();
         }
-      })
+      });
     }
   }
 
-  reset(){
+  reset() {
     this.ngOnInit();
   }
 
-  getSubs(id: number){
-    this.router.navigate(['genel-tanimlar/genel-saglik-sorunlari-alt-basliklar.html', id.toString()]);
+  getSubs(id: number) {
+    this.router.navigate([
+      'genel-tanimlar/genel-saglik-sorunlari-alt-basliklar.html',
+      id.toString(),
+    ]);
   }
-
 }
