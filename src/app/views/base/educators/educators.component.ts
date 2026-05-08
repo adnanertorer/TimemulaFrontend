345070a47f9c52132d6a0f84c25c29487128b81e
Adnan Ertörer <adnan.ertorer@indata.com.tr>
2026-05-08 10:30:31 +0300

    #feat: staffs

--- src/app/views/base/educators/educators.component.ts
+++ src/app/views/base/educators/educators.component.ts
@@ -3,55 +3,76 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app';
 import { StaffModel } from 'src/app/shared/model/staff-model';
 import { StaffService } from 'src/app/shared/services/staff.service';
 
 @Component({
   selector: 'app-educators',
   templateUrl: './educators.component.html',
-  styleUrls: ['./educators.component.css']
+  styleUrls: ['./educators.component.css'],
 })
 export class EducatorsComponent implements OnInit {
-
   educators: StaffModel[] = [];
-  displayedColumns: string[] = ['id', 'identityNumber', 'name', 'gsm', 'email', 'gender'];
-    dataSource: MatTableDataSource<StaffModel>;
-  
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-    @ViewChild(MatSort) sort: MatSort;
+  displayedColumns: string[] = [
+    'id',
+    'identityNumber',
+    'name',
+    'gsm',
+    'email',
+    'gender',
+  ];
+  dataSource: MatTableDataSource<StaffModel> | undefined;
 
-  constructor(private service: StaffService, private router: Router) { }
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  @ViewChild(MatSort) sort: MatSort | undefined;
+
+  constructor(
+    private service: StaffService,
+    private router: Router,
+    private authService: AuthService,
+  ) {}
+
+  canAccess(permissionCode: string): boolean {
+    return this.authService.hasPermission(permissionCode);
+  }
 
   ngOnInit() {
-    this.service.getTeachers().subscribe((data)=>{
+    this.service.getTeachers().subscribe((data) => {
       this.educators = data.dynamicClass as StaffModel[];
       this.dataSource = new MatTableDataSource(this.educators);
-      this.dataSource.paginator = this.paginator;
+      if (this.paginator) {
+        this.dataSource.paginator = this.paginator;
+      }
+      if (this.sort) {
         this.dataSource.sort = this.sort;
-    })
+      }
+    });
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource) {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
     }
   }
 
-  getPackages(val) {
+  getPackages(val: any) {
     this.router.navigate(['actual-hareketler/egitmen-paketleri.html/', val]);
   }
 
-  getPreparePrice(val) {
-    this.router.navigate(['actual-hareketler/egitmen-hakedis-girisi.html/', val]);
+  getPreparePrice(val: any) {
+    this.router.navigate([
+      'actual-hareketler/egitmen-hakedis-girisi.html/',
+      val,
+    ]);
   }
 
-  resetForm(){
+  resetForm() {
     this.ngOnInit();
   }
-
- 
-
 }
